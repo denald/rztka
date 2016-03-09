2b760073515f59f9f04cc25ec6ab0bec6dcc149d
Denys Lapin <dlapin@softservecom.com>
2016-03-09 18:58:20 +0200

    added notifications about current test info

--- src/test/java/rozetka/utils/TestListener.java
+++ src/test/java/rozetka/utils/TestListener.java
@@ -9,15 +9,17 @@ public class TestListener extends TestListenerAdapter{
     @Override
     public void onTestFailure(ITestResult tr){
         Reporter.log(tr.getName() + " -- test method failed\n");
+        System.out.println("FAILED");
     }
 
     @Override
     public void onTestStart(ITestResult result){
-        System.out.println("Started running test: " + result.getName());
+        System.out.print("Started running test: " + result.getName() + " ... ");
     }
 
     @Override
     public void onTestSuccess(ITestResult tr){
         Reporter.log(tr.getName() + " -- test method success\n");
+        System.out.println("SUCCESS");
     }
 }
